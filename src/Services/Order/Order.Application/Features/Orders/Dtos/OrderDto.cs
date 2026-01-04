91d6c165edb5ddb788c76e6a4cf021f9d66051f9
aytug cavdar <aytugcavdar@gmail.com>
2026-01-05 02:47:09 +0300

    Dtos fix

---
+++ src/Services/Order/Order.Application/Features/Orders/Dtos/OrderDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Order.Application.Features.Orders.Dtos;
+
+public class OrderDto
+{
+    public Guid OrderId { get; set; }
+    public Guid UserId { get; set; }
+    public decimal TotalPrice { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string Status { get; set; } // Veya Enum olarak tutmak isterseniz OrderStatus
+    public AddressDto Address { get; set; }
+    public List<OrderItemDto> OrderItems { get; set; }
+}
---
+++ src/Services/Order/Order.Application/Features/Orders/Dtos/OrderItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Order.Application.Features.Orders.Dtos;
+
+public class OrderItemDto
+{
+    public Guid Id { get; set; }
+    public Guid ProductId { get; set; }
+    public string ProductName { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+}
